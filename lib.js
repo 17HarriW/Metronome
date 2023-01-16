e6af0f91624a0eef3094b4ecd2f521eed4cedb37
William Harris <17HarriW@fulford.york.sch.uk>
2023-01-16 11:19:40 +0000

    Added ticking

--- lib.js
+++ lib.js
@@ -3,9 +3,33 @@ console.log("Setting up metronome");
 const btnDecrease = document.getElementById("btnDecreaseBPM");
 const btnIncrease = document.getElementById("btnIncreaseBPM");
 const bpmInput = document.getElementById("txtBPM");
-
+const btnStartStop = document.getElementById("btnStartStop");
+const MAX_BPM = 120;
+const MIN_BPM = 50;
+var running = false;
 // Store the beats per minute
 var bpm = 60;
+var callback = undefined;
+
+
+btnStartStop.addEventListener("click", function (e) {
+    running = !running;
+    btnStartStop.value = running ? "Stop" : "Start";
+
+    if (running) {
+        setSpeedAndStart();
+    }
+});
+
+function setSpeedAndStart() {
+    clearInterval(callback);
+    var ms = 60000 / bpm;
+    callback = setInterval(onTick, ms);
+}
+
+function onTick() {
+    console.log("Tick");
+}
 
 // Add event handlers
 bpmInput.onchange = function (e) {
@@ -18,4 +42,24 @@ bpmInput.onchange = function (e) {
 
 btnDecrease.onclick = function (e) {
     bpm -= 5;
+    updateBPM();
+}
+
+btnIncrease.onclick = function (e) {
+    bpm += 5;
+    updateBPM();
+}
+
+function updateBPM() {
+    // Range check
+    if (bpm > MAX_BPM) {
+        bpm = MAX_BPM
+    }
+    else if (bpm < MIN_BPM) {
+        bpm = MIN_BPM
+    }
+    bpmInput.value = bpm;
+    if (running) {
+        setSpeedAndStart();
+    }
 }
